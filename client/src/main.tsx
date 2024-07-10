093ea061e4da6430c4b0745183f0e91cb9c8f214
pdemeu <pdemeu.pessoal@gmail.com>
2024-07-10 14:44:00 -0300

    feat: add RecoilRoot and ToastCOntainer in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ApplicationRoutes from "./routes";
-import "./App.css"
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApplicationRoutes />
+    <RecoilRoot>
+      <ApplicationRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnFocusLoss
+      />
+    </RecoilRoot>
   </React.StrictMode>
 );
